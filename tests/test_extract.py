c62ffdc048ec64764a0a66584b71322bcc77a219
Matthew Leick-Macari <matthew.f.macari@gmail.com>
2018-08-30 22:18:09 -0500

    adding extract functions and tests

---
+++ tests/test_extract.py
@@ -0,0 +1,22 @@
+import os
+
+import pytest
+from pytest_mock import mocker
+
+from usda_parser.extract import download_db_archive, extract_db_archive
+
+
+def test_download_db_archive(temp_dir, mocker):
+    mocker.patch('requests.get')
+    ret_value = download_db_archive(output_dir=temp_dir)
+
+    assert isinstance(ret_value, str)
+    assert ret_value == os.path.join(temp_dir, 'sr28asc.zip')
+
+
+def test_extract_db_archive(temp_dir, test_archive, test_file_names):
+    extracts = extract_db_archive(test_archive)
+    
+    for key, val in extracts.items():
+        assert key in test_file_names
+        assert os.path.exists(val)
---
+++ usda_parser/config.py
@@ -0,0 +1,3 @@
+import os
+
+USDA_URL = os.environ.get('USDA_URL', 'https://www.ars.usda.gov/ARSUserFiles/80400525/Data/SR/SR28/dnload/sr28asc.zip')
